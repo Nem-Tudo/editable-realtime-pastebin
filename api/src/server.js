6c17c7ff7a839665c5a329711631ea7d6b674ff5
Nem Tudo <gf98289@gmail.com>
2026-08-16 21:38:42 -0300

    improves

--- api/src/server.js
+++ api/src/server.js
@@ -8,9 +8,7 @@ const app = express();
 const PORT = Number(process.env.PORT || 4000);
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/codepaste';
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000'
-}));
+app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 
 const pasteSchema = new mongoose.Schema({
